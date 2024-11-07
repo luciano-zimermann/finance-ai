2eeb6cb7cef4491fd28ec68a3a1cc2ba97981804
Luciano Zimermann <lucianozn16@gmail.com>
2024-11-06 23:34:37 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Subscription = () => {
+  return (
+    <div>Subscription lero lero</div>
+  )
+}
+
+export default Subscription
---
+++ app/transactions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transactions = () => {
+  return (
+    <div>Transactions lero lero</div>
+  )
+}
+
+export default Transactions
