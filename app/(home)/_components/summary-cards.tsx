f0499c938229a2210c56bff619f6f4fb4d793b59
Luciano Zimermann <lucianozn16@gmail.com>
2024-11-18 21:37:25 -0300

    feat: add summary cards

---
+++ app/(home)/_components/summary-cards.tsx
@@ -0,0 +1,75 @@
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./summary-card";
+import { db } from "@/app/_lib/prisma";
+import { TransactionType } from "@prisma/client";
+
+interface SummaryCardsProps {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCardsProps) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
+  async function getTransactionTotalByType(
+    type: TransactionType,
+  ): Promise<number> {
+    const result = await db.transaction.aggregate({
+      where: { ...where, type },
+      _sum: { amount: true },
+    });
+
+    return Number(result._sum?.amount || 0);
+  }
+
+  const depositsTotal = await getTransactionTotalByType(
+    TransactionType.DEPOSIT,
+  );
+  const investmentsTotal = await getTransactionTotalByType(
+    TransactionType.INVESTMENT,
+  );
+  const expensesTotal = await getTransactionTotalByType(
+    TransactionType.EXPENSE,
+  );
+  const balance = depositsTotal - investmentsTotal - expensesTotal;
+
+  return (
+    <div className="space-y-6">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesas"
+          amount={expensesTotal}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SummaryCards;
