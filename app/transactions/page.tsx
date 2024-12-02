5915d9ebbd8873bb97eb3854accff2f9034d0ac0
Luciano Zimermann <lucianozn16@gmail.com>
2024-12-02 18:05:48 -0300

    ensure user can not create transaction on free plan if above limit

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -7,11 +7,12 @@ import NavBar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/login");
-
+  const userCanAddTransaction = await canUserAddTransaction();
   const transactions = await db.transaction.findMany({
     where: {
       userId,
@@ -24,7 +25,7 @@ const TransactionsPage = async () => {
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
         </div>
-        <AddTransactionButton />
+        <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         <ScrollArea>
           <DataTable columns={transactionColumns} data={transactions} />
         </ScrollArea>
